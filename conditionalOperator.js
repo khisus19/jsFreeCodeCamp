23924741773684317c94791aa951b8e3dd0a9194
Jesús Álvarez <khisus19@gmail.com>
2022-05-13 10:22:06 -0500

    Conditional operator: multiple

--- conditionalOperator.js
+++ conditionalOperator.js
@@ -3,3 +3,13 @@ function minimun(x, y) {
 }
 
 minimun(16, 9);
+
+
+// Multiple conditional operator
+function checkSign(num) {
+    return (num == 0) ? "zero" 
+       : (num > 0) ? "positive"
+       : "negative"
+   }
+   
+   checkSign(10);
