e81e7b905ec1ffae6fdde9bf14086fedb4b0103d
Jesús Álvarez <khisus19@gmail.com>
2022-05-13 10:30:09 -0500

    Conditional operator: multiple 2

--- conditionalOperator.js
+++ conditionalOperator.js
@@ -9,7 +9,18 @@ minimun(16, 9);
 function checkSign(num) {
     return (num == 0) ? "zero" 
        : (num > 0) ? "positive"
-       : "negative"
+       : "negative";
    }
    
-   checkSign(10);
+console.log(checkSign(-10));
+
+// Multiple conditional operator
+function compare(num1, num2) {
+    return (num1 === num2) ? "equal" 
+       : (num1 > num2) ? `${num1} is greater than ${num2}`
+       : `${num1} is lower than ${num2}`;
+   }
+   
+console.log(compare(-10, 10));
+console.log(compare(10, 10));
+console.log(compare(15, 10));
