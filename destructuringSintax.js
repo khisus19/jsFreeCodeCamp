c81e750567eff68f0d7baa7d408352f21ecd1fb4
Jesús Álvarez <khisus19@gmail.com>
2022-05-19 10:03:57 -0500

    Destructuring sintax - Passing objects as arguments

--- destructuringSintax.js
+++ destructuringSintax.js
@@ -42,4 +42,19 @@ function removeFirstTwo(list) {
   return arr;
 }
 const arr = removeFirstTwo(source);
-console.log(arr)
+console.log(arr);
+
+// --- Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+const half = ({max, min}) => {
+    return (max + min) / 2.0;
+};
+console.log(half(stats))
